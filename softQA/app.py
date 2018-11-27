7c56d8ca3ff9050bec8e3625a938853f39f88589
NhuanTo <45367200+NhuanTo@users.noreply.github.com>
2018-11-27 08:23:33 -0700

    Update app.py

--- softQA/app.py
+++ softQA/app.py
@@ -17,9 +17,4 @@ def hello():
 
 if __name__ == '__main__':
     import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    app.run()
